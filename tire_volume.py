56b08529ec5190e3918a3cbf30a27761123df224
JuanJoseIslasLopez <jujojo97@gmail.com>
2023-09-26 02:23:27 -0300

    exceeding requirements achives

--- tire_volume.py
+++ tire_volume.py
@@ -1,7 +1,6 @@
 import math
 from datetime import datetime
 
-
 w = float(input("What is the width of the tire? "))
 a = float(input("What is the aspect ratio of the tire? "))
 d = float(input("What is the diameter of the tire? "))
@@ -14,8 +13,16 @@ current_time = datetime.now(tz=None)
 
 print(f"{current_time:%Y-%m-%d}")
 
-
 with open("volumes.txt","at") as volumes_file:
 
     print(f"{current_time:%Y-%m-%d}, {w}, {a}, {d}, {v:.2f}",file=volumes_file)
-   
+
+buy = input(f"Do you want to buy tires with the dimensions {v:.2f}? ")
+
+if buy == "yes":
+    phone = input("Can we have your phone number? ")
+    with open("volumes.txt","at") as volumes_file:
+        print(f"{phone}",file=volumes_file)
+    print("Your number has been registred, you will be contacted by one of ours salesmans, thank you for your visit!")
+else: 
+    print("thanks for your visit") 
