2f8a89b3807c6d080338a47ebd29d4076b0c3208
JuanJoseIslasLopez <jujojo97@gmail.com>
2023-09-26 01:58:49 -0300

    everthing set

--- tire_volume.py
+++ tire_volume.py
@@ -1,4 +1,6 @@
 import math
+from datetime import datetime
+
 
 w = float(input("What is the width of the tire? "))
 a = float(input("What is the aspect ratio of the tire? "))
@@ -8,3 +10,12 @@ v = (math.pi * (w **2)) * (a) * (w * a + 2540 * d) / 10000000000
 
 print (f"The approximate volume is {v:.2f}")
 
+current_time = datetime.now(tz=None)
+
+print(f"{current_time:%Y-%m-%d}")
+
+
+with open("volumes.txt","at") as volumes_file:
+
+    print(f"{current_time:%Y-%m-%d}, {w}, {a}, {d}, {v:.2f}",file=volumes_file)
+   
