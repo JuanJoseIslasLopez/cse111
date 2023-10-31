95f4327746c44d5cd1a90e33e24f4ee7f64bf734
Juan José Islas Lopez <jujojo97@gmail.com>
2023-10-31 01:56:32 -0300

    corrected details w 6

--- test_water_flow.py
+++ test_water_flow.py
@@ -1,53 +1,58 @@
+#Juan Islas
+#31/10/2023
+#Prove that I can write a Python program and write and run test functions to help me find and fix mistakes in the program.
+#Sources: CSE111 lessons
+
 from pytest import approx
-from water_flow import water_column_height, pressure_gain_from_water_height, pressure_loss_from_pipe, pressure_loss_from_fittings, reynolds_number, pressure_loss_from_pipe_reduction
+from water_flow import ji_water_column_height, ji_pressure_gain_from_water_height, ji_pressure_loss_from_pipe, ji_pressure_loss_from_fittings, ji_reynolds_number, ji_pressure_loss_from_pipe_reduction
 import pytest
 
-def test_water_column_height():
-
-    assert water_column_height(0, 0) == 0
-    assert water_column_height(0, 10) == 7.5
-    assert water_column_height(25, 0) == 25
-    assert water_column_height(48.3, 12.8) == 57.9
-
-
-def test_pressure_gain_from_water_height():
-
-    assert pressure_gain_from_water_height (0) == 0
-    assert pressure_gain_from_water_height (30.2) == approx(295.628, abs=0.001)
-    assert pressure_gain_from_water_height (50) == approx(489.450, abs=0.001)
+def test_ji_water_column_height():
+    #calls water_column_height four times to verify that it is working correctly
+    assert ji_water_column_height(0, 0) == 0
+    assert ji_water_column_height(0, 10) == 7.5
+    assert ji_water_column_height(25, 0) == 25
+    assert ji_water_column_height(48.3, 12.8) == 57.9
 
 
-def test_pressure_loss_from_pipe():
+def test_ji__pressure_gain_from_water_height():
+    #calls pressure_gain_from_water_height three times to verify that it is working correctly
+    assert ji_pressure_gain_from_water_height (0) == 0
+    assert ji_pressure_gain_from_water_height (30.2) == approx(295.628, abs=0.001)
+    assert ji_pressure_gain_from_water_height (50) == approx(489.450, abs=0.001)
 
-    assert pressure_loss_from_pipe(0.048692, 0, 0.018, 1.75) == approx(0, abs=0.001)
-    assert pressure_loss_from_pipe(0.048692, 200, 0, 1.75) == approx(0, abs=0.001)
-    assert pressure_loss_from_pipe(0.048692, 200, 0.018, 0) == approx(0, abs=0.001)
-    assert pressure_loss_from_pipe(0.048692, 200, 0.018, 1.75) == approx(-113.008, abs=0.001)
-    assert pressure_loss_from_pipe(0.048692, 200, 0.018, 1.65) == approx(-100.462, abs=0.001)
-    assert pressure_loss_from_pipe(0.28687, 1000, 0.013, 1.65) == approx(-61.576, abs=0.001)
-    assert pressure_loss_from_pipe(0.28687, 1800.75, 0.013, 1.65) == approx(-110.884, abs=0.001)
 
-def test_pressure_loss_from_fittings():
+def test_ji__pressure_loss_from_pipe():
+    #calls pressure_loss_from_pipe seven times to verify that it is working correctly
+    assert ji_pressure_loss_from_pipe(0.048692, 0, 0.018, 1.75) == approx(0, abs=0.001)
+    assert ji_pressure_loss_from_pipe(0.048692, 200, 0, 1.75) == approx(0, abs=0.001)
+    assert ji_pressure_loss_from_pipe(0.048692, 200, 0.018, 0) == approx(0, abs=0.001)
+    assert ji_pressure_loss_from_pipe(0.048692, 200, 0.018, 1.75) == approx(-113.008, abs=0.001)
+    assert ji_pressure_loss_from_pipe(0.048692, 200, 0.018, 1.65) == approx(-100.462, abs=0.001)
+    assert ji_pressure_loss_from_pipe(0.28687, 1000, 0.013, 1.65) == approx(-61.576, abs=0.001)
+    assert ji_pressure_loss_from_pipe(0.28687, 1800.75, 0.013, 1.65) == approx(-110.884, abs=0.001)
 
-    assert pressure_loss_from_fittings (0,3) == approx(0, abs=0.001)
-    assert pressure_loss_from_fittings (1.65,0) == approx(0, abs=0.001)
-    assert pressure_loss_from_fittings (1.65,2) == approx(-0.109, abs=0.001)
-    assert pressure_loss_from_fittings (1.75,2) == approx(-0.122, abs=0.001)
-    assert pressure_loss_from_fittings (1.75,5) == approx(-0.306, abs=0.001)
+def test_ji__pressure_loss_from_fittings():
+    #calls pressure_loss_from_fittings five times to verify that it is working correctly
+    assert ji_pressure_loss_from_fittings (0,3) == approx(0, abs=0.001)
+    assert ji_pressure_loss_from_fittings (1.65,0) == approx(0, abs=0.001)
+    assert ji_pressure_loss_from_fittings (1.65,2) == approx(-0.109, abs=0.001)
+    assert ji_pressure_loss_from_fittings (1.75,2) == approx(-0.122, abs=0.001)
+    assert ji_pressure_loss_from_fittings (1.75,5) == approx(-0.306, abs=0.001)
 
-def test_reynolds_number():
+def test_ji__reynolds_number():
+    #calls reynolds_number five times to verify that it is working correctly
+    assert ji_reynolds_number (0.048692,0) == approx(0, abs= 1)
+    assert ji_reynolds_number (0.048692,1.65) == approx(80069, abs= 1)
+    assert ji_reynolds_number (0.048692,1.75) == approx(84922, abs= 1)
+    assert ji_reynolds_number (0.28687,1.65) == approx(471729, abs= 1)
+    assert ji_reynolds_number (0.28687,1.75) == approx(500318, abs= 1)
 
-    assert reynolds_number (0.048692,0) == approx(0, abs= 1)
-    assert reynolds_number (0.048692,1.65) == approx(80069, abs= 1)
-    assert reynolds_number (0.048692,1.75) == approx(84922, abs= 1)
-    assert reynolds_number (0.28687,1.65) == approx(471729, abs= 1)
-    assert reynolds_number (0.28687,1.75) == approx(500318, abs= 1)
-
-def test_pressure_loss_from_pipe_reduction():
-
-    assert pressure_loss_from_pipe_reduction(0.28687, 0, 1, 0.048692)== approx(0, abs= 0.001)
-    assert pressure_loss_from_pipe_reduction(0.28687, 1.65, 471729, 0.048692)== approx(-163.744	, abs= 0.001)
-    assert pressure_loss_from_pipe_reduction(0.28687, 1.75, 500318, 0.048692)== approx(-184.182	, abs= 0.001)
+def test_ji__pressure_loss_from_pipe_reduction():
+    #calls pressure_loss_from_pipe_reduction three times to verify that it is working correctly
+    assert ji_pressure_loss_from_pipe_reduction(0.28687, 0, 1, 0.048692)== approx(0, abs= 0.001)
+    assert ji_pressure_loss_from_pipe_reduction(0.28687, 1.65, 471729, 0.048692)== approx(-163.744	, abs= 0.001)
+    assert ji_pressure_loss_from_pipe_reduction(0.28687, 1.75, 500318, 0.048692)== approx(-184.182	, abs= 0.001)
 
 pytest.main(["-v", "--tb=line", "-rN", __file__])  
 
--- water_flow.py
+++ water_flow.py
@@ -1,36 +1,42 @@
+#Juan Islas
+#31/10/2023
+#Prove that I can write a Python program and write and run test functions to help me find and fix mistakes in the program.
+#Sources: CSE111 lessons
 
-def water_column_height(tower_height, tank_height):
 
+def ji_water_column_height(tower_height, tank_height):
+   #calculates and returns the height of a column of water from a tower height and a tank wall height
     h = tower_height + (3 * tank_height) / 4
 
     return h
 
-def pressure_gain_from_water_height(height):
-
+def ji_pressure_gain_from_water_height(height):
+    #calculates and returns the pressure caused by Earth’s gravity pulling on the water stored in an elevated tank
     p = (998.2 * 9.80665 * height)/1000
 
     return p
 
-def pressure_loss_from_pipe(pipe_diameter, pipe_length, friction_factor, fluid_velocity):
-
+def ji_pressure_loss_from_pipe(pipe_diameter, pipe_length, friction_factor, fluid_velocity):
+    #calculates and returns the water pressure lost because of the friction between the water and the walls of a pipe that it flows through
     p = (- friction_factor * pipe_length * 998.2 * fluid_velocity**2) /(2000 * pipe_diameter)
 
     return p
 
-def pressure_loss_from_fittings(fluid_velocity, quantity_fittings):
+def ji_pressure_loss_from_fittings(fluid_velocity, quantity_fittings):
+     # calculates the water pressure lost because of fittings such as 45° and 90° bends that are in a pipeline
 
     p = (-0.04 * 998.2 * fluid_velocity **2 * quantity_fittings) / 2000
 
     return p
 
-def reynolds_number(hydraulic_diameter, fluid_velocity):
-
+def ji_reynolds_number(hydraulic_diameter, fluid_velocity):
+    #calculates and returns the Reynolds number for a pipe with water flowing through it
     r = 998.2 * hydraulic_diameter * fluid_velocity / 0.0010016
 
     return r
 
-def pressure_loss_from_pipe_reduction(larger_diameter,fluid_velocity, reynolds_number, smaller_diameter):
-
+def ji_pressure_loss_from_pipe_reduction(larger_diameter,fluid_velocity, reynolds_number, smaller_diameter):
+    #calculates the water pressure lost because of water moving from a pipe with a large diameter into a pipe with a smaller diameter
     k = (0.1 + (50/reynolds_number)) * ((larger_diameter/smaller_diameter)**4-1)
 
     p = (-k * 998.2 * fluid_velocity**2) / 2000
@@ -53,27 +59,27 @@ def main():
     quantity_angles = int(input("Number of 90° angles in supply pipe: "))
     length2 = float(input("Length of pipe from supply to house (meters): "))
 
-    water_height = water_column_height(tower_height, tank_height)
-    pressure = pressure_gain_from_water_height(water_height)
+    water_height = ji_water_column_height(tower_height, tank_height)
+    pressure = ji_pressure_gain_from_water_height(water_height)
 
     diameter = PVC_SCHED80_INNER_DIAMETER
     friction = PVC_SCHED80_FRICTION_FACTOR
     velocity = SUPPLY_VELOCITY
-    reynolds = reynolds_number(diameter, velocity)
-    loss = pressure_loss_from_pipe(diameter, length1, friction, velocity)
+    reynolds = ji_reynolds_number(diameter, velocity)
+    loss = ji_pressure_loss_from_pipe(diameter, length1, friction, velocity)
     pressure += loss
 
-    loss = pressure_loss_from_fittings(velocity, quantity_angles)
+    loss = ji_pressure_loss_from_fittings(velocity, quantity_angles)
     pressure += loss
 
-    loss = pressure_loss_from_pipe_reduction(diameter,
+    loss = ji_pressure_loss_from_pipe_reduction(diameter,
             velocity, reynolds, HDPE_SDR11_INNER_DIAMETER)
     pressure += loss
 
     diameter = HDPE_SDR11_INNER_DIAMETER
     friction = HDPE_SDR11_FRICTION_FACTOR
     velocity = HOUSEHOLD_VELOCITY
-    loss = pressure_loss_from_pipe(diameter, length2, friction, velocity)
+    loss = ji_pressure_loss_from_pipe(diameter, length2, friction, velocity)
     pressure += loss
 
     print(f"Pressure at house: {pressure:.1f} kilopascals")
